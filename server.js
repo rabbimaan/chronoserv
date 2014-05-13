227f4ff598d7f6ac90248af74156cc75a0e36c62
Gogs <gogitservice@gmail.com>
2014-05-13 09:34:26 +0200

    set up basic structure.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-var debug = require('debug')('chronotrack');
+
+var debug = require('debug')('server');
+var chrono = require('./lib/chrono').createServer();
+
 
 debug('startup.');
 
+chrono.setup();
+chrono.start(function(err) {
+    if (err) {
+        debug('failed to start listener.');    
+    } else {
+        debug('up and running.');
+    }
+});
 
-debug('shutdown.');
